8a8003a911667799ef952d9fc49b558a3d2efa85
KyungminPark-steck <110165529+KyungminPark-steck@users.noreply.github.com>
2023-01-23 01:04:57 +0900

    Add files via upload
    1주차

---
+++ 1주차 실습.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a = '''Hello 
+my name is Park'''
+print(a)
+
+a = 'he said, "I play the piano"'
+print(a)
+
+a= "he said, \"I play the piano\""
+
+w = "오늘도 \"날씨\"가 좋네요. 내일도 \"날씨\"가 좋다고 합니다."
+print(w)
+
+t = "오늘은 '날씨' 가 좋네요.\n내일도 '날씨'가 좋다고 합니다."
+print(t)
+
+a = 'pine'
+b = 'apple'
+print(a+b) 
+
+a = 'straw'
+b = ' berry'
+print(a+b) 
+
+
+print("오늘은"+str(10)+"일 입니디")
+
+print("오늘은"+ '10'+"일 입니다.")
+
+a = str(10)
+print("오늘은 a일 입니다")
+
+print("top",10,"songs")
+print("top"+"10"+"songs")
+a = 'top' + '10'+ 'songs'
+b = 'top',10,'songs'
+print(a)
+print(b)
+
+
+a = '*'
+b = ' '
+
+print(a*1+"\n"+a*2+"\n"+a*3+"\n"+a*4)
+
+print(b*3+a*1+"\n"+b*2+a*3+"\n"+b*1+a*5+"\n"+a*7)
+
+s2 = "good job!"
+print(len(s2))
+
+s = "abcdefghi"
+print(len(s))
+
+s1 = "This is a computer"
+print(s1.split())
+
+
+s2 = "2022/3/17"
+print(s2.split("/"))
+
+text = '''She follows it down a rabbit hole when suddenly she
+ falls a long way to a curious hall with many locked doors of all
+sizes'''
+
+print(text.upper())
+print(text.lower())
+
+print(text.split())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
